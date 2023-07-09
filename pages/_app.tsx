498d0f6d596af635f57bae212e69b168dbfc99e4
Mahdi Karimi <mhdi_kr@outlook.com>
2023-07-09 03:40:02 +0330

    feat: new header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,38 @@
 import "../styles/globals.css";
 import "../styles/dracula.css";
 import "../styles/fonts.css";
-import "../styles/typography.css"
+import "../styles/typography.css";
 
 import type { AppProps } from "next/app";
 import Footer from "../components/Footer";
 import Link from "next/link";
+import Image from "next/image";
 import Script from "next/script";
 
 function ApplicationRoot({ Component, pageProps }: AppProps) {
     const navigationItems = [
         {
-            title: 'Home',
-            href: '/'
+            title: "Home",
+            href: "/",
         },
         {
-            title: 'Blog',
-            href: '/blog'
+            title: "Blog",
+            href: "/blog",
         },
         {
-            title: 'Talks',
-            href: '/talks'
+            title: "Talks",
+            href: "/talks",
         },
         {
-            title: 'Contact',
-            href: '/contact'
+            title: "Contact",
+            href: "/contact",
         },
         // TODO: add RSS feature
         // {
         //     title: 'RSS',
         //     href: '/rss'
         // },
-    ]
+    ];
     return (
         <>
             <Script
@@ -39,17 +40,28 @@ function ApplicationRoot({ Component, pageProps }: AppProps) {
                 data-domain="mahdikarimi.com"
                 src="https://plausible.robio.ir/js/script.js"
             />
-            <section className="container px-4 md:px-4 lg:px-28 mx-auto flex flex-col w-full md:flex-row items-start">
-                <aside className="order-last md:order-first w-full md:w-1/5 md:h-screen py-8 px-4 relative md:sticky md:top-0">
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img className="rounded-lg shadow-lg mb-4 w-full" src="https://github.com/mhdi-kr.png" alt="" />
-                    <h1 className="text-center text-xl font-bold mb-4">Mahdi Karimi</h1>
-                    <nav>
-                        <ul className="text-center block w-full">
+            <section className="container px-4 md:px-4 lg:px-28 mx-auto flex flex-col w-full items-start">
+                <header className="w-full container py-8 px-4 flex items-center justify-center md:justify-between">
+                    <section className="flex items-center md:flex-row flex-col gap-2 md:gap-4">
+                        <Image
+                            role="img"
+                            src="https://github.com/mhdi-kr.png"
+                            height={128}
+                            width={128}
+                            alt="Mahdi Karimi - Software engineer"
+                            className="rounded-full"
+                        />
+                        <section className="text-center md:text-left">
+                            <h1 className="md:text-5xl font-bold">Mahdi Karimi</h1>
+                            <span className="md:text-3xl font-thin">Software Engineer</span>
+                        </section>
+                    </section>
+                    <nav className="hidden md:block" role="navigation">
+                        <ul className="flex gap-3">
                             {
                                 navigationItems.map((link) =>
                                     <Link key={link.title} passHref={true} href={`${link.href}`}>
-                                        <li className="dark:bg-zinc-800 bg-zinc-50 transition-all dark:hover:bg-zinc-700 hover:shadow-md rounded mb-2 py-2 cursor-pointer">
+                                        <li tabIndex={1} className="dark:bg-zinc-900 px-2 bg-zinc-50 transition-all dark:hover:bg-zinc-700 hover:shadow-md rounded mb-2 py-2 cursor-pointer">
                                             {link.title}
                                         </li>
                                     </Link>
@@ -57,12 +69,27 @@ function ApplicationRoot({ Component, pageProps }: AppProps) {
                             }
                         </ul>
                     </nav>
-                    <Footer />
-                </aside>
-                <main className="order-first md:order-last w-full md:w-4/5 py-8 px-4">
+                </header>
+
+                <main className="w-full container py-8 px-4">
                     <Component {...pageProps} />
                 </main>
-            </section></>
+
+                <nav className="flex md:hidden w-full items-center justify-center mb-3" role="navigation">
+                        <ul className="flex flex-col gap-3 w-full">
+                            {
+                                navigationItems.map((link) =>
+                                    <Link key={link.title} passHref={true} href={`${link.href}`}>
+                                        <li tabIndex={1} className="dark:bg-zinc-800 text-center p-2 w-full bg-zinc-50 transition-all dark:hover:bg-zinc-700 hover:shadow-md rounded cursor-pointer">
+                                            {link.title}
+                                        </li>
+                                    </Link>
+                                )
+                            }
+                        </ul>
+                    </nav>
+            </section>
+        </>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,16 @@ export default function Home({ posts }: { posts: IPost[] }) {
                 <title>Mahdi Karimi - Home</title>
                 <meta name="description" content="Mahdi Karimi - Home" />
             </Head>
-
-            <p>Everyone has been blessed to have certain limited number of keystrokes in their lifetime. sooner or later you and I will run out of them, but when you write things people can read and learn from, that&apos;s when you multiply your keystrokes and make them eternal.</p>
-            <p>I am <a className="text-secondary-blog" referrerPolicy={'no-referrer'} href="https://resume.io/r/kCFLoVvw3">Mahdi Karimi</a>, computer engineer and hardware hacker born in Iran.</p>
-            <p>Writing, sharing my experiences through well-written and thought-out blog posts gives me a great sense of pleasure.</p>
-            <p>Knowing that someone can read them and actually find it useful is the feeling I am looking for alongside my professional career.</p>
+            <article>
+                <p className="md:text-2xl md:leading-9">
+                    I am <a className="text-yellow-500 font-semibold" referrerPolicy={'no-referrer'} href="https://www.linkedin.com/in/mhdi-kr/">Mahdi Karimi</a>, an Iranian-born computer engineer and hardware hacker.
+                </p>
+                <p className="md:text-2xl md:leading-9">With nearly five years of experience in the software engineering industry, I have worked at prominent Iranian tech companies like Alibaba Travels Co. and Snapp! Group.</p>
+                <p className="md:text-2xl md:leading-9">I consider myself a T-shaped software engineer, with extensive expertise in frontend technologies such as Vue.js and React.js, as well as frontend tooling. I am also well-versed in designing microservices and backend development. Additionally, I have a keen interest in internet of things, machine learning, and computer science.</p>
+                <p className="md:text-2xl md:leading-9">Each person has a finite number of keystrokes in their lifetime. Eventually, both you and I will exhaust them. However, when you write things that people can read and learn from, you multiply the impact of your keystrokes, making them everlasting.</p>
+                <p className="md:text-2xl md:leading-9">Writing and sharing my experiences through well-crafted and thoughtful blog posts brings me immense pleasure. During my free time, I enjoy reading books, writing technical articles, and contributing to the open-source community.</p>
+                <p className="md:text-2xl md:leading-9">The sense of fulfillment I seek, alongside my professional career, is knowing that someone can read my work and find it genuinely useful.</p>
+            </article>
         </>
     );
 }
